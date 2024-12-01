df7d47776427f3fd280eef2105c50c5cdebff340
nlkishore <nlaxmikishore@gmail.com>
2024-12-01 18:12:17 +0800

    modified

--- callhierarchy1/CallHierarchyRule.java
+++ callhierarchy1/CallHierarchyRule.java
@@ -1,55 +1,25 @@
-import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceDeclaration;
-import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
-import net.sourceforge.pmd.lang.java.ast.ASTMethodCall;
-import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
+//package com.example;
 
-import java.util.*;
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
+import net.sourceforge.pmd.lang.java.ast.ASTPrimaryExpression;
+import net.sourceforge.pmd.RuleContext;
 
 public class CallHierarchyRule extends AbstractJavaRule {
 
-    // Map to store the call hierarchy: Caller -> List of Callees
-    private final Map<String, List<String>> callHierarchy = new HashMap<>();
-
     @Override
-    public Object visit(ASTClassOrInterfaceDeclaration node, Object data) {
-        String className = node.getImage(); // Get the class name
+    public Object visit(ASTPrimaryExpression node, Object data) {
+        RuleContext context = (RuleContext) data;
 
-        // Process all methods in the class
-        List<ASTMethodDeclaration> methods = node.findDescendantsOfType(ASTMethodDeclaration.class);
-        for (ASTMethodDeclaration method : methods) {
-            String methodName = method.getName();
-            String caller = className + "." + methodName;
+        if (node.jjtGetNumChildren() > 0) {
+            String methodName = node.jjtGetChild(0).getImage();
+            if (methodName != null) {
+                System.out.println("Method call: " + methodName);
 
-            // Find all method calls within this method
-            List<ASTMethodCall> methodCalls = method.findDescendantsOfType(ASTMethodCall.class);
-            for (ASTMethodCall methodCall : methodCalls) {
-                String callee = methodCall.getMethodName();
-                if (callee != null) {
-                    callHierarchy.computeIfAbsent(caller, k -> new ArrayList<>()).add(callee);
-                }
+                // Process method call using an external library
+                ExternalLibraryUtil.processMethodCall(methodName);
             }
         }
 
         return super.visit(node, data);
-    }
-
-    @Override
-    public Object visit(ASTMethodDeclaration node, Object data) {
-        // Optional: Add additional logic to handle methods directly
-        return super.visit(node, data);
-    }
-
-    @Override
-    public void end(RuleContext ctx) {
-        // Output the call hierarchy
-        callHierarchy.forEach((caller, callees) -> {
-            StringBuilder output = new StringBuilder(caller + " calls: ");
-            output.append(String.join(", ", callees));
-            ctx.getReport().addRuleViolation(
-                createViolation(ctx, 0, output.toString())
-            );
-        });
-
-        super.end(ctx);
     }
 }
