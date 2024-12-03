626dd031263e1c0dd7d98aa94bdc4a90991b64ff
nlkishore <nlaxmikishore@gmail.com>
2024-12-03 13:16:36 +0800

    Sonar Lint CLI

---
+++ SonarLint-CLI/SonarLintRunner.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import org.sonarsource.sonarlint.core.client.api.common.analysis.AnalysisResults;
+import org.sonarsource.sonarlint.core.client.api.common.analysis.Issue;
+import org.sonarsource.sonarlint.core.client.api.standalone.StandaloneGlobalConfiguration;
+import org.sonarsource.sonarlint.core.client.api.standalone.StandaloneSonarLintEngine;
+import org.sonarsource.sonarlint.core.client.api.standalone.StandaloneSonarLintEngineFactory;
+import org.sonarsource.sonarlint.core.client.api.standalone.StandaloneSonarLintProject;
+
+import java.nio.file.Paths;
+
+public class SonarLintRunner {
+    public static void main(String[] args) {
+        StandaloneGlobalConfiguration globalConfig = StandaloneGlobalConfiguration.builder()
+                .addPlugin(Paths.get("path/to/sonar-java-plugin.jar"))
+                .setWorkDir(Paths.get("path/to/workdir"))
+                .build();
+
+        try (StandaloneSonarLintEngine engine = StandaloneSonarLintEngineFactory.create(globalConfig)) {
+            StandaloneSonarLintProject project = engine.createStandaloneProject(
+                    Paths.get("path/to/project"),
+                    "project-key");
+
+            AnalysisResults results = engine.analyze(project);
+
+            for (Issue issue : results.getIssues()) {
+                System.out.println(issue);
+            }
+        }
+    }
+}
+
