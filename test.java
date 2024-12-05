d27d7316b82fe2227104f5412d02fb3ecd571088
nlkishore <nlaxmikishore@gmail.com>
2024-12-05 23:05:31 +0800

    Added PMD CommandLine

--- test.java
+++ test.java
@@ -5,3 +5,4 @@ public class test {
 }
     git config --global user.name"nlkishore"
         git config--global user.email"nlaxmikishore@gmail.com"
+        java -jar pmd-cli-6.0.1.jar -d src -R custom-xpath-rule.xml -f text
